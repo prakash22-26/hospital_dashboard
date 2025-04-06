333a535e622da3b6618d71177660e3406c537b46
prakash22-26 <prakashkumarshah156@gmail.com>
2025-04-06 16:48:23 +0530

    Update Patient.js

--- frontend/src/components/Patient.js
+++ frontend/src/components/Patient.js
@@ -91,7 +91,7 @@ export default function PatientDashboard() {
     fetchAppointments();
     fetchCareTeam();
     fetchPrescriptions();
-  }, []);
+  },);
 
   const fetchPatientProfile = async () => {
     try {
